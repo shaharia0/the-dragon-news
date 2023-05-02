1eb9bf078d9b71e23a1fb51787e27816436b4ab9
shaharia0 <shaharia35-1964@diu.edu.bd>
2023-05-02 13:00:51 +0600

    starting a dragon news website and completed header ffunction and section

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC7ax6MjQlsuINxkuaSdFr0iTzGLM0zhs8",
+  authDomain: "the-news-dragon-e7136.firebaseapp.com",
+  projectId: "the-news-dragon-e7136",
+  storageBucket: "the-news-dragon-e7136.appspot.com",
+  messagingSenderId: "522469477721",
+  appId: "1:522469477721:web:3e5a5761e2d16b12bc5048"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      <p className='text-center'><small>copyright &copy; of my news 2023</small></p>
+      {/* total distance make in tailwind : min-h-[calc(100vh - yourTotalGap)] */}
+    </div>
+  );
+};
+
+export default Footer;
