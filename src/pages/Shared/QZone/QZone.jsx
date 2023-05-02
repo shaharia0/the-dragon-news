fe91580addf4a267037e0a138abe63bd0743adf3
shaharia0 <shaharia35-1964@diu.edu.bd>
2023-05-03 01:30:50 +0600

    added left nav and load data from server side

--- src/pages/Shared/QZone/QZone.jsx
+++ src/pages/Shared/QZone/QZone.jsx
@@ -5,11 +5,13 @@ import qzone3 from '../../../assets/qZone3.png'
 
 const QZone = () => {
   return (
-    <div className='bg-secondary text-center mt-2 rounded'>
+    <div className='bg-secondary text-center py-4 mt-2 rounded'>
       <h4>Q-Zone</h4>
+      <div>
       <img src={qzone1} alt="" />
       <img src={qzone2} alt="" />
       <img src={qzone3} alt="" />
+      </div>
     </div>
   );
 };
--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Button, ListGroup } from "react-bootstrap";
 import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 import QZone from "../QZone/QZone";
+import bg from '../../../assets/bg.png'
+import bg1 from '../../../assets/bg1.png'
 
 const RightNav = () => {
   return (
     <div>
-      <h4>Login with</h4>
+      <h4 className="mt-4">Login with</h4>
       <Button className="mb-2" variant="outline-primary">
         <FaGoogle></FaGoogle> Login with google
       </Button>
@@ -22,6 +24,17 @@ const RightNav = () => {
         </ListGroup>
       </div>
       <QZone></QZone>
+      <div className="mb-5 mt-3 rounded d-flex justify-content-center" style={{position: 'relative'}}>
+        <img src={bg} alt="" />
+        <img src={bg1} alt="" style={{position: 'absolute', top: '0', left: '0', zIndex: '1', opacity: '50%'}}/>
+        <div style={{position: 'absolute', top: '0', left: '0', zIndex: '1'}}>
+          <h4 className="text-white text-center fs-1 mt-5">Create an Amazing Newspaper</h4>
+           <p className="fs-5 mt-3 text-white text-center p-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+           <div className="d-flex justify-content-center">
+            <Button variant="danger" size="lg">Learn More</Button>
+           </div>
+        </div>
+      </div>
     </div>
   );
 };
