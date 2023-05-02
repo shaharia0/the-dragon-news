99e906dfa3462bc1bc6a22644a1103d0903d5df6
shaharia0 <shaharia35-1964@diu.edu.bd>
2023-05-02 15:24:59 +0600

    added right nav qzone section and sopial icons

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React from "react";
+import { Button, ListGroup } from "react-bootstrap";
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+import QZone from "../QZone/QZone";
 
 const RightNav = () => {
   return (
     <div>
-      <h2>Right Navigation</h2>
+      <h4>Login with</h4>
+      <Button className="mb-2" variant="outline-primary">
+        <FaGoogle></FaGoogle> Login with google
+      </Button>
+      <Button variant="outline-secondary">
+        <FaGithub></FaGithub> Login with github
+      </Button>
+      <div>
+        <h4 className="mt-2">Find Us On</h4>
+        <ListGroup>
+          <ListGroup.Item><FaFacebook></FaFacebook> Facebook</ListGroup.Item>
+          <ListGroup.Item><FaTwitter></FaTwitter> Twitter</ListGroup.Item>
+          <ListGroup.Item><FaInstagram></FaInstagram> Instagram</ListGroup.Item>
+        </ListGroup>
+      </div>
+      <QZone></QZone>
     </div>
   );
 };
