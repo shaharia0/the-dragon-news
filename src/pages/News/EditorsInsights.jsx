a289a331e2827db77f3cfd54e7560408084e3c8e
shaharia0 <shaharia35-1964@diu.edu.bd>
2023-05-04 11:56:30 +0600

    added main section lower card and leftnav lower card

--- src/pages/News/EditorsInsights.jsx
+++ src/pages/News/EditorsInsights.jsx
@@ -1,31 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Card, Col, Row } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import first from '../../../assets/1.png'
-import second from '../../../assets/2.png'
-import third from '../../../assets/3.png'
+import first from '../../assets/1.png'
+import second from '../../assets/2.png'
+import third from '../../assets/3.png'
 
-const LeftNav = () => {
-  const [categories, setCategories] = useState([]);
-  useEffect(()=>{
-    fetch('http://localhost:5000/categories')
-    .then(res => res.json())
-    .then(data => setCategories(data))
-    .catch(error=> console.error(error))
-  },[])
+const EditorsInsights = () => {
   return (
-    <div>
-      <h4 className='mt-4'>All Category</h4>
-      <div className='ps-4'>
-      {
-        categories.map(category => <p
-        key={category.id}
-        >
-          <Link to={`/category/${category.id}`} className='text-decoration-none text-secondary'>{category.name}</Link>
-        </p>)
-      }
-      </div>
-      <Row xs={1} md={2} lg={1} className="g-4 mt-4">
+    <Row xs={1} md={2} lg={3} className="g-4 mt-4">
       <Col>
         <Card>
           <Card.Img variant="top" src={first} />
@@ -66,8 +47,7 @@ const LeftNav = () => {
         </Card>
       </Col>
   </Row>
-    </div>
   );
 };
 
-export default LeftNav;
+export default EditorsInsights;
